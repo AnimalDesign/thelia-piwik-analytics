2a6aa28b26fb6b0555de1e7eecd2659a14d78d46
Matthias Esterl <me@madcity.at>
2015-12-05 22:09:02 +0100

    Added Tracking for Category and Product pages

--- Hook/FrontHook.php
+++ Hook/FrontHook.php
@@ -5,52 +5,100 @@ namespace HookPiwikAnalytics\Hook;
 use Thelia\Core\Event\Hook\HookRenderEvent;
 use Thelia\Core\Hook\BaseHook;
 use Thelia\Model\ConfigQuery;
+use Thelia\Model\ProductQuery;
+use Thelia\Model\ProductCategoryQuery;
+use Thelia\Model\CategoryQuery;
 
 /**
- * Class FrontHook
+ * Class FrontHook.
  *
  * @author ANIMAL <studio@animal.at>
  */
 class FrontHook extends BaseHook
 {
+    /* Include tracking bug
+     */
     public function onMainBodyBottom(HookRenderEvent $event)
     {
         $url = ConfigQuery::read('hookpiwikanalytics_url', false);
         $website_id = ConfigQuery::read('hookpiwikanalytics_website_id', false);
-        
-        if($code = $this->generateTrackingCode($url, $website_id)) {
+        $options = array();
+
+        switch ($this->getRequest()->get('_view')) {
+            case 'category':
+                $categoryId = $this->getRequest()->get('category_id');
+
+                $defaultCategory = CategoryQuery::create()
+                    ->findPk($categoryId);
+
+                $options[] = array(
+                    'setEcommerceView',
+                    false, // SKU or ID
+                    false, // Name
+                    $defaultCategory->getTitle(), // Category
+                );
+                break;
+            
+            case 'product':
+                $productId = $this->getRequest()->getProductId();
+                $product = ProductQuery::create()
+                    ->findPk($productId);
+                
+                if($defaultCategoryId = $product->getDefaultCategoryId()) {
+                    $defaultCategory = CategoryQuery::create()
+                        ->findPk($defaultCategoryId);
+                }
+                
+                $options[] = array(
+                    'setEcommerceView',
+                    ($product->getRef() ? $product->getRef() : $product->getId()), // SKU or ID
+                    $product->getTitle(), // Name
+                    (isset($defaultCategory) ? $defaultCategory->getTitle() : false), // Category
+                    false, // Price
+                );
+                break;
+        }
+
+        if ($code = $this->generateTrackingCode($url, $website_id, $options)) {
             $event->add($code);
         }
     }
-    
-    private function generateTrackingCode($url, $website_id) {
-        if(!empty($url) && is_numeric($website_id)) {
+
+    private function generateTrackingCode($url, $website_id, $options)
+    {
+        if (!empty($url) && is_numeric($website_id)) {
             // remove http, https, add // before and / after url
             $url = preg_replace('#^https?://#', '', $url);
             $url = '//'.ltrim($url, '//');
-            $url = rtrim($url, '/') . '/';
-            
+            $url = rtrim($url, '/').'/';
+
             $code = '
-            <!-- Piwik -->
             <script type="text/javascript">
-              var _paq = _paq || [];
-              _paq.push([\'trackPageView\']);
-              _paq.push([\'enableLinkTracking\']);
-              (function() {
-                var u="'.$url.'";
-                _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
-                _paq.push([\'setSiteId\', '.$website_id.']);
-                var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
-                g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'piwik.js\'; s.parentNode.insertBefore(g,s);
-              })();
+                var _paq = _paq || [];';
+            
+            foreach ($options as $option) {
+                $code .= '
+                    _paq.push('.json_encode($option).');
+                ';
+            }
+            
+            $code .= '
+                _paq.push([\'trackPageView\']);
+                _paq.push([\'enableLinkTracking\']);
+                (function() {
+                    var u="'.$url.'";
+                    _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
+                    _paq.push([\'setSiteId\', '.$website_id.']);
+                    var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
+                    g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'piwik.js\'; s.parentNode.insertBefore(g,s);
+                })();
             </script>
             <noscript><p><img src="'.$url.'piwik.php?idsite='.$website_id.'" style="border:0;" alt="" /></p></noscript>
-            <!-- End Piwik Code -->
             ';
 
             return $code;
         }
-        
+
         return false;
     }
 }
